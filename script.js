ea2a518848652bc33db4b260c90003b60e115c1b
korius kussa wenda <koriuswenda@gmail.com>
2024-06-11 17:57:57 +0700

    tambah : file index.html, style.css, script.js

---
+++ script.js
@@ -0,0 +1,26 @@
+// document
+// .getElementById('pesan')
+// .textContent = 'Selamat Datang !';
+
+// document
+//.getElementById('pesan')
+// .querySelector('#pesan')
+// .textContent = 'Selamat Datang !';
+
+// document
+// .getElementById('selamat')
+// .textContent = 'Happy Coding !';
+
+// var selamatElement = document.querySelector("selamat");
+// selamatElement.textContent = "Happy Coding !";
+
+ var getElementBaru = document.createElement("strong");
+ getElementBaru.textContent = "di SaCode Community !";
+
+
+
+document
+.querySelector("selamat")
+.textContent = "Happy Coding";
+
+
